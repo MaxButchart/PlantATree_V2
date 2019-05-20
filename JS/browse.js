3176036781af29d5f297a4d20f6dea3332975a2d
MaxButchart <max.butchart@live.com>
2019-05-20 12:16:06 +1200

    Added Javascript files as well as a page for browsing trees

---
+++ JS/browse.js
@@ -0,0 +1,15 @@
+$('.dropdown-menu a').click(function(){
+    $('#type').text($(this).text());
+  });
+
+$('.dropdown-menu a').click(function(){
+    $('#height').text($(this).text());
+  });
+
+$('.dropdown-menu a').click(function(){
+    $('#sun').text($(this).text());
+  });
+
+$('.dropdown-menu a').click(function(){
+      $('#soil').text($(this).text());
+  });
